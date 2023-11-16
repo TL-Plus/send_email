ef0a9a5618b70b3002c1aacc890df658ec5b2e0f
lanlt <thelaniq@gmail.com>
2023-11-16 17:55:11 +0700

    update send email

--- includes/config.php
+++ includes/config.php
@@ -1,17 +1,17 @@
 <?php
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_PORT', 587);
-define('SMTP_USERNAME', 'lan.lt@diginext.com.vn');
-define('SMTP_PASSWORD', 'sfin ddqk ckch vdyc');
-define('SENDER_EMAIL', 'lan.lt@diginext.com.vn');
+define('SMTP_USERNAME', 'SMTP_USERNAME');
+define('SMTP_PASSWORD', 'SMTP_PASSWORD');
+define('SENDER_EMAIL', 'SENDER_EMAIL');
 
 define('RECIPIENTS', [
-    'thelaniq@gmail.com',
+    'RECIPIENTS_EMAIL',
     // Add more email addresses as needed
 ]);
 
 // Database connection details
-define('DB_HOSTNAME', '192.168.227.148');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'thelan11');
-define('DB_DATABASE', 'VoiceReport');
+define('DB_HOSTNAME', 'DB_HOSTNAME');
+define('DB_USERNAME', 'DB_USERNAME');
+define('DB_PASSWORD', 'DB_PASSWORD');
+define('DB_DATABASE', 'DB_DATABASE');
