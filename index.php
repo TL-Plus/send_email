ffc6929454263685802362b57764da5995ec6946
lanlt <thelaniq@gmail.com>
2023-11-17 20:34:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,8 @@ require 'includes/email_functions.php';
 require 'includes/query_functions.php';
 require 'email_notifications.php';
 
-$recipients = RECIPIENTS;
-
 // Call function to send email notification for 17-day warning
-sendEmailForDays($query1, 'report_warning_17_days.xlsx', '17-Day Warning - Excel File', $recipients);
+sendEmailForDays($query1, 'report_warning_17_days.xlsx', '17-Day Warning - Excel File', RECIPIENTS);
 
 // Call function to send email notification for termination on the 19th day
-sendEmailForDays($query2, 'report_termination_19_days.xlsx', 'Termination on 19th Day - Excel File', $recipients);
+sendEmailForDays($query2, 'report_termination_19_days.xlsx', 'Termination on 19th Day - Excel File', RECIPIENTS);
