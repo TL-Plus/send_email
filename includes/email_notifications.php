f6eb16c52fca713e4a42c1b92884089383caeb8b
lanlt <thelaniq@gmail.com>
2023-11-22 16:27:43 +0700

    update

--- includes/email_notifications.php
+++ includes/email_notifications.php
@@ -1,13 +1,45 @@
 <?php
-require 'includes/export_excel.php';
-require_once 'includes/config.php';
 
-// Function to send email notification
-function sendEmailForDays($query, $attachment, $subject, $recipients)
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+require_once 'config.php';
+
+function sendEmailNotification($attachmentPath, $subject, $body, $recipients)
 {
+    $mail = new PHPMailer();
+
     try {
-        exportToExcel($query, $attachment);
-        sendEmailNotification("/root/{$attachment}", $subject, "Excel Files for {$subject}", $recipients);
+        // Server settings
+        $mail->IsSMTP();
+        $mail->CharSet = 'UTF-8';
+        $mail->Host = SMTP_HOST;
+        $mail->SMTPDebug = 0;
+        $mail->SMTPAuth = true;
+        $mail->Port = SMTP_PORT;
+        $mail->Username = SMTP_USERNAME;
+        $mail->Password = SMTP_PASSWORD;
+
+        $mail->setFrom(SENDER_EMAIL);
+
+        // Add each recipient to the email
+        $recipients = explode(',', RECIPIENTS);
+        foreach ($recipients as $recipient) {
+            $mail->addAddress(trim($recipient));
+        }
+
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->addAttachment($attachmentPath);
+
+        if (!$mail->send()) {
+            throw new Exception('Mailer Error: ' . $mail->ErrorInfo);
+        }
+
+        echo 'The email message was sent.';
     } catch (Exception $e) {
         echo 'Error: ' . $e->getMessage();
     }
