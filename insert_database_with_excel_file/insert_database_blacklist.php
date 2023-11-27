bed6967fa12d3df734873ba77581d9715f1a4e36
lanlt <thelaniq@gmail.com>
2023-11-27 16:29:21 +0700

    insert database with excel file

---
+++ insert_database_with_excel_file/insert_database_blacklist.php
@@ -0,0 +1,59 @@
+<?php
+
+require 'send_email/includes/config.php';
+require 'send_email/includes/database_connection.php';
+require 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+function insertDataFromExcel($inputFileName)
+{
+    // Create a new PhpSpreadsheet object
+    try {
+        $spreadsheet = IOFactory::load($inputFileName);
+    } catch (Exception $e) {
+        die('Error loading Excel file: ' . $e->getMessage());
+    }
+
+    // Establish a database connection
+    $conn = connectDatabase(DB_HOSTNAME_DIGINEXT, DB_USERNAME_DIGINEXT, DB_PASSWORD_DIGINEXT, DB_DATABASE_DIGINEXT);
+
+    // Prepare the SQL statement
+    $sql = "INSERT INTO `BlackList` (`msisdn`, `telco`, `shortcode`, `info`, `mo_time`, `cmd_code`, `error_code`, `error_desc`, `updated_at`, `created_at`) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())";
+
+    // Prepare the SQL statement once outside the loop for better performance
+    if ($stmt = $conn->prepare($sql)) {
+        $stmt->bind_param("ssssssss", $msisdn, $telco, $shortcode, $info, $mo_time, $cmd_code, $error_code, $error_desc);
+
+        // Loop through each row in the Excel file
+        foreach ($spreadsheet->getActiveSheet()->getRowIterator() as $row) {
+            // Skip the header row
+            if ($row->getRowIndex() == 1) {
+                continue;
+            }
+
+            // Get cell values
+            $data = [];
+            foreach ($row->getCellIterator() as $cell) {
+                $data[] = $cell->getValue();
+            }
+
+            // Assign values to variables
+            list($msisdn, $telco, $shortcode, $info, $mo_time, $cmd_code, $error_code, $error_desc) = $data;
+
+            // Execute the prepared statement
+            if (!$stmt->execute()) {
+                echo "Error: " . $stmt->error . "<br>";
+            }
+        }
+
+        // Close the prepared statement
+        $stmt->close();
+    } else {
+        echo "Error preparing SQL statement: " . $conn->error . "<br>";
+    }
+
+    $conn->close();
+    echo "Data inserted successfully!";
+}
