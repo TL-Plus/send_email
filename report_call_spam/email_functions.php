0ba9b014e67d0c3b0caeb845dda38b5d6920f54b
lanlt <thelaniq@gmail.com>
2023-11-27 13:34:56 +0700

    update send email diginext

--- report_call_spam/email_functions.php
+++ report_call_spam/email_functions.php
@@ -4,16 +4,6 @@ require 'includes/export_excel_diginext.php';
 require_once 'send_email/includes/config.php';
 
 // Function to send email notification
-function sendEmailForDays($query1, $query2, $attachment, $subject, $recipients)
-{
-    try {
-        exportToExcel($query1, $query2, $attachment);
-        sendEmailNotification("/root/{$attachment}", $subject, "Excel Files for {$subject}", $recipients);
-    } catch (Exception $e) {
-        echo 'Error: ' . $e->getMessage();
-    }
-}
-
 function sendEmailForDaysDiginext($query, $attachment, $subject, $recipients)
 {
     try {
