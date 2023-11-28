b5e62680136c930a216128576529eb34b1e85cc4
lanlt <thelaniq@gmail.com>
2023-11-28 21:58:10 +0700

    Update export_excel.php

--- includes/export_excel.php
+++ includes/export_excel.php
@@ -2,6 +2,9 @@
 require 'vendor/autoload.php';
 require_once 'config.php';
 
+use PhpOffice\PhpSpreadsheet\Worksheet\Table;
+use PhpOffice\PhpSpreadsheet\Worksheet\Table\TableStyle;
+
 function exportToExcel($sql, $header, $filename)
 {
     $result = connectAndQueryDatabase($sql, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
@@ -14,8 +17,22 @@ function exportToExcel($sql, $header, $filename)
         // Set the title in the Excel file
         $sheet->fromArray([$header], NULL, 'A1');
 
+        // Make header row bold
+        $headerStyle = $sheet->getStyle('A1:' . $sheet->getHighestColumn() . '1');
+        $headerStyle->getFont()->setBold(true);
+
+        // Calculate header column widths based on the length of the longest string
+        $column = 'A';
+        $headerWidths = [];
+        foreach ($header as $headerItem) {
+            $headerWidths[$column] = strlen($headerItem) + 2;
+            $column++;
+        }
+
         // Data from the database
         $row = 2;
+        $columnWidths = [];
+
         while ($row_data = $result->fetch_assoc()) {
             $column = 'A';
             foreach ($row_data as $key => $value) {
@@ -23,11 +40,43 @@ function exportToExcel($sql, $header, $filename)
                     // Handle time formats
                     $value = date('Y-m-d H:i:s', strtotime($value));
                 }
-                $sheet->setCellValue($column . $row, $value);
+
+                // Set cell value explicitly to handle different data types
+                $sheet->setCellValueExplicit($column . $row, $value, PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);
+
+                // Calculate column width based on the length of the longest string
+                $columnWidths[$column] = max(strlen($value) + 2, isset($columnWidths[$column]) ? $columnWidths[$column] : 0);
+
                 $column++;
             }
             $row++;
         }
+
+        // Update header column widths with the maximum of header and data lengths
+        foreach ($headerWidths as $column => $width) {
+            $headerWidths[$column] = max($headerWidths[$column], isset($columnWidths[$column]) ? $columnWidths[$column] : 0);
+        }
+
+        // Set column widths
+        foreach ($headerWidths as $column => $width) {
+            $sheet->getColumnDimension($column)->setWidth($width);
+        }
+
+        // Create a table
+        $lastColumn = $sheet->getHighestColumn();
+        $table = new Table("A1:$lastColumn$row");
+        $table->setRange('A1:' . $lastColumn . $row);
+
+        // Create a table style
+        $tableStyle = new TableStyle();
+        $tableStyle->setTheme(TableStyle::TABLE_STYLE_MEDIUM9);
+        $tableStyle->setShowRowStripes(true);
+
+        // Apply table style to the table
+        $table->setStyle($tableStyle);
+
+        // Add the table to the worksheet
+        $sheet->addTable($table);
 
         // Save the Excel file
         $writer = new PhpOffice\PhpSpreadsheet\Writer\Xlsx($spreadsheet);
