0ff77c6179c9f3202e58dd3feb55bd98caa31497
lanlt <thelaniq@gmail.com>
2023-11-29 14:55:11 +0700

    update report call spam today and yesterday of diginext

--- report_call_spam/index_diginext.php
+++ report_call_spam/index_diginext.php
@@ -9,10 +9,18 @@ $header = [
     'TimeAction', 'Day', 'CustomerName', 'ContractCode', 'Caller', 'Callee', 'SL'
 ];
 
-// Prepare email details
+// today
+$today = date('Y_m_d');
+$attachment = "Report_call_spam_DIGINEXT_$today.xlsx";
+$subject = "Report Call Spam DIGINEXT ($today)";
+
+// Call function to send email notification warning payment today
+sendEmailForDays($query_report_call_spam_by_number_contract_next, $header, $attachment, $subject, RECIPIENTS);
+
+// yesterday
 $yesterday = date('Y_m_d', strtotime('-1 days'));
 $attachment = "Report_call_spam_DIGINEXT_$yesterday.xlsx";
 $subject = "Report Call Spam DIGINEXT ($yesterday)";
 
-// Call function to send email notification warning payment
-sendEmailForDays($query_report_call_spam_by_number_contract_next, $header, $attachment, $subject, RECIPIENTS);
+// Call function to send email notification warning payment yesterday
+sendEmailForDays($query_report_call_spam_by_number_contract_next_bk, $header, $attachment, $subject, RECIPIENTS);
