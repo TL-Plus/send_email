15d5e470cebd711663cb3d9e85f6bd084e64bb75
iamlanlt <thelaniq@gmail.com>
2023-12-26 11:15:48 +0700

    update report DNC

--- report_do_not_call/index_diginext.php
+++ report_do_not_call/index_diginext.php
@@ -6,16 +6,28 @@ require 'send_email/includes/send_telegram_message.php';
 
 
 // query_report_call_spam_by_number_contract_next
-$query_report_do_not_call_blacklist = "SELECT `msisdn`, `telco`, 
-`shortcode`, `info`, `mo_time`, `cmd_code`, 
-`error_code`, `error_desc`, `updated_at`, `created_at` 
-FROM `BlackList`
-WHERE DAY(updated_at) = DATE_FORMAT(CURDATE() - INTERVAL 1 DAY, '%d')
-ORDER BY `BlackList`.`updated_at` ASC";
+$query_report_do_not_call_blacklist = "SELECT 
+`msisdn`, 
+CONCAT(DATE_FORMAT(`updated_at`, '%y-%m-%d'), '|', `cmd_code`) AS `day_cmd_code`,
+`telco`, 
+`shortcode`, 
+`info`, 
+`mo_time`, 
+`cmd_code`, 
+`error_code`, 
+`error_desc`, 
+`updated_at`, 
+`created_at`
+FROM 
+  `BlackList`
+WHERE 
+  DATE(updated_at) = CURDATE() - INTERVAL 1 DAY
+ORDER BY 
+  `BlackList`.`updated_at` ASC";
 
 // Define Excel header
 $header = [
-    'msisdn', 'telco', 'shortcode', 'info', 'mo_time', 'cmd_code', 'error_code', 'error_desc', 'updated_at', 'created_at'
+    'msisdn', 'day_cmd_code', 'telco', 'shortcode', 'info', 'mo_time', 'cmd_code', 'error_code', 'error_desc', 'updated_at', 'created_at'
 ];
 
 // Define $dbName, $chatId
@@ -23,7 +35,7 @@ $dbName = $_ENV['DB_DATABASE_BLACKLIST'];
 $chatId = $_ENV['TELEGRAM_CHAT_ID'];
 
 // yesterday
-$yesterday = date('Y_m_d');
+$yesterday = date('Y_m_d', strtotime('-1 days'));
 $attachment = "Report_DNC_$yesterday.xlsx";
 $subject = "Report DNC DIGINEXT ($yesterday)";
 
