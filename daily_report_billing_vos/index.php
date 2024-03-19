01e960d8212a39e03719218d68b986f904c39d15
iamlanlt <thelaniq@gmail.com>
2024-03-19 08:40:59 +0700

    update daily report billing-vos

--- daily_report_billing_vos/index.php
+++ daily_report_billing_vos/index.php
@@ -25,18 +25,17 @@ $spreadsheetId = '1hZ5df7fQpKRnYOrK7OMpwmiv0sWD4dYRbcazk3I_ZJQ';
 
 date_default_timezone_set("Asia/Ho_Chi_Minh");
 
-// $yesterday = date("Y-m-11");
 $yesterday = date("Y-m-d", strtotime("-1 day"));
 $table_name_cdr = "cdr" . date('Ymd', strtotime($yesterday));
 $table_name_cdrdsip = "cdrdsip" . date('Ym', strtotime($yesterday));
 $currentTime = date('H:i:s d-m-Y');
 $current_month = date('m/Y');
 
-// Truy vấn dữ liệu từ cơ sở dữ liệu
+// cdr
 $query_cdr = "SELECT DATE(time) AS Time, COUNT(*) as TotalCallCDR, SUM(duration) as TotalDurationCDR 
         FROM $table_name_cdr
         WHERE callee_gw LIKE 'RT_0%' AND duration > 0 AND DATE(time) = '$yesterday'";
-            
+
 $result_cdr = connectAndQueryDatabase(
     $query_cdr,
     $_ENV['DB_HOSTNAME_DIGINEXT'],
@@ -46,32 +45,27 @@ $result_cdr = connectAndQueryDatabase(
 );
 
 if ($result_cdr->num_rows > 0) {
-    // Iterate over each row of data and insert into Google Sheets
-    while ($row = $result_cdr->fetch_assoc()) {
-        $values = [
-            [$yesterday, $row['TotalCallCDR'], $row['TotalDurationCDR']]
-        ]; // Sửa đổi tên cột
+    $row = $result_cdr->fetch_assoc();
 
-        // Tìm vị trí của cột time trong bảng Google Sheets (giả sử là cột A)
-        $timeColumnIndex = 'A';
-
-        // Lấy số hàng cuối cùng của bảng
-        $response = $service->spreadsheets_values->get($spreadsheetId, "Billing-Vos $current_month!A:A");
-        $lastRow = count($response->getValues()) + 1;
-
-        // Đặt phạm vi chèn dữ liệu
-        $range = "Billing-Vos $current_month!A{$lastRow}";
-
-        // Gửi yêu cầu chèn dữ liệu
-        $requestBody = new Google_Service_Sheets_ValueRange([
-            'values' => $values
-        ]);
-        $service->spreadsheets_values->append($spreadsheetId, $range, $requestBody, ['valueInputOption' => 'USER_ENTERED']);
-    }
+    $values = [
+        [$yesterday, $row['TotalCallCDR'], $row['TotalDurationCDR']]
+    ];
 } else {
-    echo "0 results";
+    $values = [
+        [$yesterday, 0, 0]
+    ];
 }
 
+$timeColumnIndex = 'A';
+$response = $service->spreadsheets_values->get($spreadsheetId, "Billing-Vos $current_month!A:A");
+$lastRow = count($response->getValues()) + 1;
+$range = "Billing-Vos $current_month!A{$lastRow}";
+$requestBody = new Google_Service_Sheets_ValueRange([
+    'values' => $values
+]);
+$service->spreadsheets_values->append($spreadsheetId, $range, $requestBody, ['valueInputOption' => 'USER_ENTERED']);
+
+// cdrdsip
 $query_cdrdsip = "SELECT DATE(time) AS Time, COUNT(*) AS TotalCallCDRDSIP, SUM(duration) AS TotalDurationCDRDSIP 
             FROM $table_name_cdrdsip
             WHERE callee_gw LIKE 'RT_DIGISIP_VINAPHONE' AND duration > 0 
@@ -87,94 +81,33 @@ $result_cdrdsip = connectAndQueryDatabase(
 );
 
 if ($result_cdrdsip->num_rows > 0) {
-    // Iterate over each row of data and insert into Google Sheets
-    while ($row = $result_cdrdsip->fetch_assoc()) {
-        $values = [
-            [$row['TotalCallCDRDSIP'], $row['TotalDurationCDRDSIP'], $yesterday]
-        ]; // Sửa đổi tên cột
+    $row = $result_cdrdsip->fetch_assoc();
 
-        // Tìm vị trí của cột time trong bảng Google Sheets (giả sử là cột A)
-        $timeColumnIndex = 'L';
-
-        // Lấy số hàng cuối cùng của bảng
-        $response = $service->spreadsheets_values->get($spreadsheetId, "Billing-Vos $current_month!L:L");
-        $lastRow = count($response->getValues()) + 1;
-
-        // Đặt phạm vi chèn dữ liệu
-        $range = "Billing-Vos $current_month!L{$lastRow}";
-
-        // Gửi yêu cầu chèn dữ liệu
-        $requestBody = new Google_Service_Sheets_ValueRange([
-            'values' => $values
-        ]);
-        $service->spreadsheets_values->append($spreadsheetId, $range, $requestBody, ['valueInputOption' => 'USER_ENTERED']);
-    }
+    $values = [
+        [$row['TotalCallCDRDSIP'], $row['TotalDurationCDRDSIP'], $yesterday]
+    ];
 } else {
-    echo "0 results";
+    $values = [
+        [0, 0, $yesterday]
+    ];
 }
 
+$timeColumnIndex = 'L';
+$response = $service->spreadsheets_values->get($spreadsheetId, "Billing-Vos $current_month!L:L");
+$lastRow = count($response->getValues()) + 1;
+$range = "Billing-Vos $current_month!L{$lastRow}";
+$requestBody = new Google_Service_Sheets_ValueRange([
+    'values' => $values
+]);
+$service->spreadsheets_values->append($spreadsheetId, $range, $requestBody, ['valueInputOption' => 'USER_ENTERED']);
+
+// send telegram
 $textMessage = "Dữ liệu Báo Cáo Billing-VOS DIGINEXT đã được cập nhật xong vào lúc: $currentTime!" . PHP_EOL
-  . "Bạn hãy vào link: https://docs.google.com/spreadsheets/d/1hZ5df7fQpKRnYOrK7OMpwmiv0sWD4dYRbcazk3I_ZJQ/edit#gid=559014398 để xem và cập nhật thêm dữ liệu!" . PHP_EOL
-  . "link chính: https://docs.google.com/spreadsheets/d/1Z3O7Hy_uxpPXjjikFoJwsgqisYwXasD6mLd4g3AiIFM/edit#gid=1198928396";
+    . "Bạn hãy vào link: https://docs.google.com/spreadsheets/d/1hZ5df7fQpKRnYOrK7OMpwmiv0sWD4dYRbcazk3I_ZJQ/edit#gid=559014398 để xem và cập nhật thêm dữ liệu!" . PHP_EOL
+    . "link chính: https://docs.google.com/spreadsheets/d/1Z3O7Hy_uxpPXjjikFoJwsgqisYwXasD6mLd4g3AiIFM/edit#gid=1198928396";
 
 sendTelegramMessage(
     $textMessage,
     $_ENV['TELEGRAM_BOT_TOKEN_DIGINEXT'],
     $_ENV['TELEGRAM_CHAT_ID']
-  );
-
-
-
-// // Get the first day of the month
-// $firstDayOfMonth = date("Y-m-01");
-// // Get yesterday's date
-// $endDate = date("Y-m-d", strtotime("-1 day"));
-
-// $currentDate = $firstDayOfMonth;
-// $allRows = [];
-
-// // Loop through each day from the first day of the month to yesterday
-// while ($currentDate <= $endDate) {
-//     $table_name = "cdr" . date('Ymd', strtotime($currentDate));
-
-//     // Truy vấn dữ liệu từ cơ sở dữ liệu cho mỗi ngày
-//     $query = "SELECT DATE(`time`) AS Time, COUNT(*) as TotalCall, SUM(duration) as TotalDuration 
-//             FROM $table_name
-//             WHERE callee_gw LIKE 'RT_0%' AND duration > 0 AND DATE(`time`) = '$currentDate'";
-
-//     $result = connectAndQueryDatabase(
-//         $query,
-//         $_ENV['DB_HOSTNAME_DIGINEXT'],
-//         $_ENV['DB_USERNAME_DIGINEXT'],
-//         $_ENV['DB_PASSWORD_DIGINEXT'],
-//         $_ENV['DB_DATABASE_VOICEREPORT'],
-//     );
-
-//     if ($result->num_rows > 0) {
-//         // Fetch each row of data and append to $allRows array
-//         while ($row = $result->fetch_assoc()) {
-//             $allRows[] = [
-//                 $currentDate, // Date
-//                 $row['TotalCall'], // TotalCall
-//                 $row['TotalDuration'] // TotalDuration
-//             ];
-//         }
-//     }
-
-//     // Move to the previous day
-//     $currentDate = date("Y-m-d", strtotime("-1 day", strtotime($currentDate)));
-// }
-
-// // Check if there is any data to insert
-// if (!empty($allRows)) {
-//     // Đặt phạm vi chèn dữ liệu
-//     $range = "Trang tính2!A1"; // Assuming the data will be inserted starting from cell A1
-
-//     // Gửi yêu cầu chèn dữ liệu
-//     $requestBody = new Google_Service_Sheets_ValueRange([
-//         'values' => $allRows
-//     ]);
-//     $service->spreadsheets_values->append($spreadsheetId, $range, $requestBody, ['valueInputOption' => 'USER_ENTERED']);
-// } else {
-//     echo "0 results";
-// } 
+);
